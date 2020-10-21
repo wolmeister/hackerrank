5916af3331eee94204db1f28c236050201a976bd
wolmeister <wolmeistervictor@gmail.com>
2020-10-21 17:06:19 -0300

    Create counting-valleys.js

---
+++ counting-valleys.js
@@ -0,0 +1,15 @@
+let level = 0;
+let valleys = 0;
+
+for (let i = 0; i < steps; i +=1) {
+    if (path[i] === 'U') {
+        level += 1;
+    } else {
+        if (level === 0) {
+            valleys += 1;
+        }
+        level -= 1;
+    }
+}
+
+return valleys;
