95c409110824d6f4143c7be2dc08ef0fb3ba1ea8
wolmeister <wolmeistervictor@gmail.com>
2020-10-21 17:08:23 -0300

    Update counting-valleys.js

--- counting-valleys.js
+++ counting-valleys.js
@@ -1,15 +1,17 @@
-let level = 0;
-let valleys = 0;
+function countingValleys(steps, path) {
+    let level = 0;
+    let valleys = 0;
 
-for (let i = 0; i < steps; i +=1) {
-    if (path[i] === 'U') {
-        level += 1;
-    } else {
-        if (level === 0) {
-            valleys += 1;
+    for (let i = 0; i < steps; i +=1) {
+        if (path[i] === 'U') {
+            level += 1;
+        } else {
+            if (level === 0) {
+                valleys += 1;
+            }
+            level -= 1;
         }
-        level -= 1;
     }
-}
 
-return valleys;
+    return valleys;
+}
